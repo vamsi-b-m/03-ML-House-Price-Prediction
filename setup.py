939a3ef59f8c71bae59e19777765450d88497ed0
vamsi-b-m <vamsi.b.x@gmail.com>
2023-11-02 22:10:46 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
+from typing import List
+
+
 
 # Declaring Varibles for setup functions
 PROJECT_NAME = "house-price-predictor"
 VERSION="0.0.1"
 AUTHOR="Vamsi Batta"
 DESCRIPTION='First ML Project'
+REQUIREMENTS_FILE  = "requirements.txt"
+
+def get_requirements_list()->List[str]:
+    with open(REQUIREMENTS_FILE) as requirements_file:
+        requirements_file.readlines() 
+
 
 
 setup(
@@ -12,5 +21,6 @@ setup(
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=['housing']
+    packages=['housing'],
+    install_requires=get_requirements_list()
 )
