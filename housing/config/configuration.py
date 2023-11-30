50933840f0c8376fabb68364cfd35baeac50a1fd
vamsi-b-m <vamsi.b.x@gmail.com>
2023-11-30 17:02:26 +0530

    Completed the Configuration.py

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -56,21 +56,32 @@ class Configuration:
     def get_data_validation_config(self) -> DataValidationConfig:
         try:
             artifact_dir = self.training_pipeline_config.artifact_dir
+
             data_validation_artifact_dir = os.path.join(
                 artifact_dir,
                 DATA_VALIDATION_ARTIFACT_DIR,
                 self.time_stamp
             )
-            data_validation_config = self.config_info[DATA_VALIDATION_CONFIG_KEY]
-            schema_dir = os.path.join(
-                data_validation_artifact_dir, 
-                data_validation_config[DATA_VALIDATION_SCHEMA_DIR]
-            )
-            schema_file_name = data_validation_config[DATA_VALIDATION_SCHEMA_FILE_NAME]
-            report_file_name = data_validation_config[DATA_VALIDATION_REPORT_FILE_NAME]
-            report_page_file_name = data_validation_config[DATA_VALIDATION_REPORT_PAGE_FILE_NAME]
 
-            data_validation_config = DataValidationConfig(schema_dir=schema_dir, schema_file_name=schema_file_name, report_file_name=report_file_name, report_page_file_name=report_page_file_name)
+            data_validation_config = self.config_info[DATA_VALIDATION_CONFIG_KEY]
+
+            schema_file_path = os.path.join(
+                ROOT_DIR,
+                data_validation_config[DATA_VALIDATION_SCHEMA_DIR_KEY],
+                data_validation_config[DATA_VALIDATION_SCHEMA_FILE_NAME_KEY]
+            )
+
+            report_file_path = os.path.join(
+                data_validation_artifact_dir,
+                data_validation_config[DATA_VALIDATION_REPORT_FILE_NAME_KEY]
+            )
+
+            report_page_file_path = os.path.join(
+                data_validation_artifact_dir,
+                data_validation_config[DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY]
+            )
+
+            data_validation_config = DataValidationConfig(schema_file_path=schema_file_path, report_file_path=report_file_path, report_page_file_path=report_page_file_path)
             logging.info(f"Data Validation Config : {data_validation_config}")
             return data_validation_config
         
@@ -80,31 +91,36 @@ class Configuration:
     def get_data_transformation_config(self) -> DataTransformationConfig:
         try:
             artifact_dir = self.training_pipeline_config.artifact_dir
+
             data_transformation_artifact_dir = os.path.join(
                 artifact_dir,
                 DATA_TRANSFORMATION_ARTIFACT_DIR,
                 self.time_stamp
             )
+
             data_transformation_config = self.config_info[DATA_TRANSFORMATION_CONFIG_KEY]
-            add_bedroom_per_room = data_transformation_config[DATA_TRANSFORMATION_ADD_BEDROOM_PER_ROOM]
-            transformed_dir = os.path.join(
-                data_transformation_artifact_dir,
-                data_transformation_config[DATA_TRANSFORMATION_TRANFORMED_DIR]
-            )
+
+            add_bedroom_per_room = data_transformation_config[DATA_TRANSFORMATION_ADD_BEDROOM_PER_ROOM_KEY]
+            
             transformed_train_dir = os.path.join(
-                transformed_dir,
-                data_transformation_config[DATA_TRANSFORMATION_TRANFORMED_TRAIN_DIR]
-            )
-            transformed_test_dir = os.path.join(
-                transformed_dir,
-                data_transformation_config[DATA_TRANSFORMATION_TRANFORMED_TEST_DIR]
-            )
-            preprocessing_dir = os.path.join(
                 data_transformation_artifact_dir,
-                data_transformation_config[DATA_TRANSFORMATION_PREPROCESSING_DIR]
+                data_transformation_config[DATA_TRANSFORMATION_DIR_NAME_KEY],
+                data_transformation_config[DATA_TRANSFORMATION_TRAIN_DIR_NAME_KEY]
             )
-            preprocessed_object_file_name = data_transformation_config[DATA_TRANSFORMATION_PREPROCESSED_OBJECT_FILE_NAME]
-            data_transformation_config = DataTransformationConfig(add_bedroom_per_room=add_bedroom_per_room, transformed_dir=transformed_dir, transformed_train_dir=transformed_train_dir, transformed_test_dir=transformed_test_dir,preprocessing_dir=preprocessing_dir, preprocessed_object_file_name=preprocessed_object_file_name)
+
+            transformed_test_dir = os.path.join(
+                data_transformation_artifact_dir,
+                data_transformation_config[DATA_TRANSFORMATION_DIR_NAME_KEY],
+                data_transformation_config[DATA_TRANSFORMATION_TEST_DIR_NAME_KEY]
+            )
+
+            preprocessed_object_file_path = os.path.join(
+                data_transformation_artifact_dir,
+                data_transformation_config[DATA_TRANSFORMATION_PREPROCESSING_DIR_KEY],
+                data_transformation_config[DATA_TRANSFORMATION_PREPROCESSED_OBJECT_FILE_NAME_KEY]
+            )
+            
+            data_transformation_config = DataTransformationConfig(add_bedroom_per_room=add_bedroom_per_room, transformed_train_dir=transformed_train_dir, transformed_test_dir=transformed_test_dir, preprocessed_object_file_path=preprocessed_object_file_path)
             logging.info(f"Data Transformation Config : {data_transformation_config}")
             return data_transformation_config
         except Exception as e:
@@ -113,33 +129,80 @@ class Configuration:
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         try:
             artifact_dir = self.training_pipeline_config.artifact_dir
-            data_validation_artifact_dir = os.path.join(
+
+            model_trainer_artifact_dir = os.path.join(
                 artifact_dir,
-                DATA_VALIDATION_ARTIFACT_DIR,
+                MODEL_TRAINER_ARTIFACT_DIR,
                 self.time_stamp
             )
+
+            model_trainer_config = self.config_info[MODEL_TRAINER_CONFIG_KEY]
+            
+            trained_model_file_path = os.path.join(
+                model_trainer_artifact_dir,
+                model_trainer_config[MODEL_TRAINER_TRAINED_MODEL_DIR_KEY],
+                model_trainer_config[MODEL_TRAINER_TRAINED_MODEL_CONFIG_FILE_NAME_KEY]
+            )
+            
+            model_config_file_path = os.path.join(
+                model_trainer_artifact_dir,
+                model_trainer_config[MODEL_TRAINER_TRAINED_MODEL_CONFIG_DIR_KEY],
+                model_trainer_config[MODEL_TRAINER_TRAINED_MODEL_CONFIG_FILE_NAME_KEY]
+            )
+
+            base_accuracy = model_trainer_config[MODEL_TRAINER_BASE_ACCURACY_KEY]
+
+            model_trainer_config = ModelTrainerConfig(trained_model_file_path=trained_model_file_path, model_config_file_path=model_config_file_path, base_accuracy=base_accuracy)
+            logging.info(f"Model Trainer Config : {model_trainer_config}")
+            return model_trainer_config
         except Exception as e:
             raise HousingException(e, sys) from e
 
     def get_model_evaluation_config(self) -> ModelEvaluationConfig:
         try:
             artifact_dir = self.training_pipeline_config.artifact_dir
-            data_validation_artifact_dir = os.path.join(
+
+            model_evaluation_artifact_dir = os.path.join(
                 artifact_dir,
-                DATA_VALIDATION_ARTIFACT_DIR,
-                self.time_stamp
+                MODEL_EVALUATION_ARTIFACT_DIR,
             )
+
+            model_evaluation_config = self.config_info[MODEL_EVALUATION_CONFIG_KEY]
+
+            model_evaluation_file_path = os.path.join(
+                model_evaluation_artifact_dir,
+                model_evaluation_config[MODEL_EVALUATION_FILE_NAME_KEY]
+            )
+
+            model_evaluation_config = ModelEvaluationConfig(model_evaluation_file_path=model_evaluation_file_path, timestamp=self.time_stamp)
+            logging.info(f"Model Evaluation Config : {model_evaluation_config}")
+            return model_evaluation_config
         except Exception as e:
             raise HousingException(e, sys) from e
 
     def get_model_pusher_config(self) -> ModelPusherConfig:
         try:
+            time_stamp = f"{datetime.now().strftime('%Y%m%d%H%M%S')}"
+
             artifact_dir = self.training_pipeline_config.artifact_dir
-            data_validation_artifact_dir = os.path.join(
+
+            model_pusher_artifact_dir = os.path.join(
                 artifact_dir,
-                DATA_VALIDATION_ARTIFACT_DIR,
+                MODEL_PUSHER_ARTIFACT_DIR,
                 self.time_stamp
             )
+
+            model_pusher_config = self.config_info[MODEL_PUSHER_CONFIG_KEY]
+
+            model_export_dir_path = os.path.join(
+                ROOT_DIR, 
+                model_pusher_config[MODEL_PUSHER_EXPORT_DIR_KEY],
+                time_stamp
+            )
+            
+            model_pusher_config = ModelPusherConfig(model_export_dir_path=model_export_dir_path)
+            logging.info(f"Model Pusher Config : {model_pusher_config}")
+            return model_pusher_config
         except Exception as e:
             raise HousingException(e, sys) from e
 
--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -27,19 +27,36 @@ DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir"
 # Data Validation Related Variables
 DATA_VALIDATION_CONFIG_KEY = "data_validation_config"
 DATA_VALIDATION_ARTIFACT_DIR = "data_validation"
-DATA_VALIDATION_SCHEMA_DIR = "schema_dir"
-DATA_VALIDATION_SCHEMA_FILE_NAME = "schema_file_name"
-DATA_VALIDATION_REPORT_FILE_NAME = "report_file_name"
-DATA_VALIDATION_REPORT_PAGE_FILE_NAME = "report_page_file_name"
+DATA_VALIDATION_SCHEMA_DIR_KEY = "schema_dir"
+DATA_VALIDATION_SCHEMA_FILE_NAME_KEY = "schema_file_name"
+DATA_VALIDATION_REPORT_FILE_NAME_KEY = "report_file_name"
+DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY = "report_page_file_name"
 
 # Data Transformation Related Variables
 DATA_TRANSFORMATION_CONFIG_KEY = "data_transformation_config"
 DATA_TRANSFORMATION_ARTIFACT_DIR = "data_transformation"
-DATA_TRANSFORMATION_ADD_BEDROOM_PER_ROOM = "add_bedroom_per_room"
-DATA_TRANSFORMATION_TRANFORMED_DIR = "transformed_dir"
-DATA_TRANSFORMATION_TRANFORMED_TRAIN_DIR = "transformed_train_dir"
-DATA_TRANSFORMATION_TRANFORMED_TEST_DIR = "transformed_test_dir"
-DATA_TRANSFORMATION_PREPROCESSING_DIR = "preprocessing_dir"
-DATA_TRANSFORMATION_PREPROCESSED_OBJECT_FILE_NAME = "preprocessed_object_file_name"
+DATA_TRANSFORMATION_ADD_BEDROOM_PER_ROOM_KEY = "add_bedroom_per_room"
+DATA_TRANSFORMATION_DIR_NAME_KEY = "transformed_dir"
+DATA_TRANSFORMATION_TRAIN_DIR_NAME_KEY = "transformed_train_dir"
+DATA_TRANSFORMATION_TEST_DIR_NAME_KEY = "transformed_test_dir"
+DATA_TRANSFORMATION_PREPROCESSING_DIR_KEY = "preprocessing_dir"
+DATA_TRANSFORMATION_PREPROCESSED_OBJECT_FILE_NAME_KEY = "preprocessed_object_file_name"
 
+# Model Trainer Related Variables
+MODEL_TRAINER_CONFIG_KEY = "model_trainer_config"
+MODEL_TRAINER_ARTIFACT_DIR = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR_KEY = "trained_model_dir"
+MODEL_TRAINER_MODEL_FILE_NAME_KEY = "model_file_name"
+MODEL_TRAINER_BASE_ACCURACY_KEY = "base_accuracy"
+MODEL_TRAINER_TRAINED_MODEL_CONFIG_DIR_KEY = "model_config_dir"
+MODEL_TRAINER_TRAINED_MODEL_CONFIG_FILE_NAME_KEY = "model_config_file_name"
 
+# Model Evaluation Related Variables
+MODEL_EVALUATION_CONFIG_KEY = "model_evaluation_config"
+MODEL_EVALUATION_ARTIFACT_DIR = "model_evaluation"
+MODEL_EVALUATION_FILE_NAME_KEY = "model_evaluation_file_name"
+
+# Model Pusher Related Variables
+MODEL_PUSHER_CONFIG_KEY = "model_pusher_config"
+MODEL_PUSHER_ARTIFACT_DIR = "model_pusher"
+MODEL_PUSHER_EXPORT_DIR_KEY = "model_export_dir"
