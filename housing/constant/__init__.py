22d7397b67f92b087660f2c08e43154ca50288c4
vamsi-b-m <vamsi.b.x@gmail.com>
2023-11-30 14:08:09 +0530

    Created a New Tester Notebook

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -32,4 +32,14 @@ DATA_VALIDATION_SCHEMA_FILE_NAME = "schema_file_name"
 DATA_VALIDATION_REPORT_FILE_NAME = "report_file_name"
 DATA_VALIDATION_REPORT_PAGE_FILE_NAME = "report_page_file_name"
 
+# Data Transformation Related Variables
+DATA_TRANSFORMATION_CONFIG_KEY = "data_transformation_config"
+DATA_TRANSFORMATION_ARTIFACT_DIR = "data_transformation"
+DATA_TRANSFORMATION_ADD_BEDROOM_PER_ROOM = "add_bedroom_per_room"
+DATA_TRANSFORMATION_TRANFORMED_DIR = "transformed_dir"
+DATA_TRANSFORMATION_TRANFORMED_TRAIN_DIR = "transformed_train_dir"
+DATA_TRANSFORMATION_TRANFORMED_TEST_DIR = "transformed_test_dir"
+DATA_TRANSFORMATION_PREPROCESSING_DIR = "preprocessing_dir"
+DATA_TRANSFORMATION_PREPROCESSED_OBJECT_FILE_NAME = "preprocessed_object_file_name"
+
 
