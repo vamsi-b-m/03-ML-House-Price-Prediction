dabdbe66f26bd63fbc1becf2b189c208f1b0a85b
vamsi-b-m <vamsi.b.x@gmail.com>
2024-01-05 12:10:09 +0530

    Updated

--- housing/entity/artifact_entity.py
+++ housing/entity/artifact_entity.py
@@ -2,3 +2,4 @@ from collections import namedtuple
 
 DataIngestionArtifact = namedtuple("DataIngestionArtfifact",["train_file_path", "test_file_path", "is_ingested", "message"])
 DataValidationArtifact = namedtuple("DataValidationArtifact", ["schema_file_path", "report_file_path", "report_page_file_path", "is_validated", "message"])
+DataTranformationArtifact = namedtuple("DataTransformationArtifact",["is_transformed", "message", "transformed_train_file_path", "transformed_test_file_path", "preprocessed_object_file_path"])
