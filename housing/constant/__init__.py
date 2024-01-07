b6fcf39ecc8abd1ed22dd66f959abc135465ee67
vamsi-b-m <vamsi.b.x@gmail.com>
2024-01-07 16:56:47 +0530

    Completed Data Transformation

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -41,6 +41,16 @@ DATA_TRANSFORMATION_TEST_DIR_NAME_KEY = "transformed_test_dir"
 DATA_TRANSFORMATION_PREPROCESSING_DIR_KEY = "preprocessing_dir"
 DATA_TRANSFORMATION_PREPROCESSED_OBJECT_FILE_NAME_KEY = "preprocessed_object_file_name"
 
+COLUMN_TOTAL_ROOMS = "total_rooms"
+COLUMN_POPULATION = "population"
+COLUMN_HOUSEHOLDS = "households"
+COLUMN_TOTAL_BEDROOM = "total_bedrooms"
+DATASET_SCHEMA_COLUMNS_KEY = "columns"
+NUMERICAL_COLUMN_KEY = "numerical_columns"
+CATEGORICAL_COLUMN_KEY = "categorical_columns"
+TARGET_COLUMN_KEY = "target_column"
+
+
 # Model Trainer Related Variables
 MODEL_TRAINER_CONFIG_KEY = "model_trainer_config"
 MODEL_TRAINER_ARTIFACT_DIR = "model_trainer"
--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -4,13 +4,14 @@ from housing.config.configuration import Configuration
 from housing.logger import logging
 from housing.exception import HousingException
 
-from housing.entity.artifact_entity import DataIngestionArtifact
-from housing.entity.artifact_entity import DataValidationArtifact
+from housing.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
 from housing.entity.config_entity import DataIngestionConfig
 from housing.components.data_ingestion import DataIngestion
 from housing.components.data_validation import DataValidation
+from housing.components.data_transformer import DataTransformation
 
 class Pipeline:
+    
     def __init__(self, config : Configuration = Configuration()) -> None:
         try:
             self.config = config
@@ -31,8 +32,12 @@ class Pipeline:
         except Exception as e:
             raise HousingException(e, sys) from e
 
-    def start_data_transformation(self):
-        pass
+    def start_data_transformation(self, data_ingestion_artifact:DataIngestionArtifact, data_validation_artifact:DataValidationArtifact) -> DataTransformationArtifact:
+        try:
+            data_transformation = DataTransformation(data_transformation_config=self.config.get_data_transformation_config(), data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
+            return data_transformation.initiate_data_transformation()
+        except Exception as e:
+            raise HousingException(e, sys) from e
 
     def start_model_trainer(self):
         pass
@@ -43,7 +48,8 @@ class Pipeline:
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
         except Exception as e:
             raise HousingException(e, sys) from e
         
